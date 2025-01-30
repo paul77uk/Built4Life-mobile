89f00ad63d15db50f0ec214e3650e5d296b01f1c
paul vickers <paul77uk@gmail.com>
2025-01-30 16:39:21 +0000

    updated readme with basic info for now

--- app/src/main/java/com/example/built4life/ui/home/HomePage.kt
+++ app/src/main/java/com/example/built4life/ui/home/HomePage.kt
@@ -7,6 +7,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -15,9 +19,11 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.built4life.R
 import com.example.built4life.customcomposables.AppBar
 
 @Composable
@@ -25,7 +31,16 @@ fun HomePage(
     modifier: Modifier = Modifier, viewModel: HomeViewModel = hiltViewModel()
 ) {
     val uiState by viewModel.homeUiState.collectAsState()
-    Scaffold(topBar = { AppBar("Built4Life") }) { innerPadding ->
+    Scaffold(topBar = { AppBar(stringResource(R.string.app_name)) },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {},
+                containerColor = Color(0xFFFE9900),
+            ) {
+                Icon(Icons.Filled.Add, "Add Program", tint = Color.White)
+
+            }
+        }) { innerPadding ->
         LazyColumn(
             modifier = Modifier
                 .padding(innerPadding)
